a4c045238ebfdfe29586b4917bc8563480b3badf
Ben Taljaard <benfrancotaljaard@gmail.com>
2021-10-18 13:10:07 +0200

    updated release script

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
+
 ./mvnw --batch-mode release:prepare
-./mvnw --batch-mode release:perform
+./mvnw --batch-mode -Drepo.id=qiot-central -Drepo.login=$ARTIFACTORY_USER -Drepo.pwd=$ARTIFACTORY_PASSWORD release:perform
 if [[ "$?" -ne 0 ]] ; then
   ./mvnw --batch-mode release:rollback
 fi
