89069f283f4c83d39d878445f96007e2611c426e
Ben Taljaard <benfrancotaljaard@gmail.com>
2021-10-18 14:17:28 +0200

    fix params, rather use local settings.xml

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 ./mvnw --batch-mode release:prepare
-./mvnw --batch-mode -Dqiot-central.username=$ARTIFACTORY_USER -Dqiot-central.password=$ARTIFACTORY_PASSWORD release:perform
+./mvnw --batch-mode release:perform
 if [[ "$?" -ne 0 ]] ; then
   ./mvnw --batch-mode release:rollback
 fi
