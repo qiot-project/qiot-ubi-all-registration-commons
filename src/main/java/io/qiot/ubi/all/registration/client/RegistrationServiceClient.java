1bc5275fa97dc068c52c8c19d639dda22687f19c
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-05 18:28:36 +0100

    [RELEASE] Github Actions cleanup; code cleanup

--- src/main/java/io/qiot/ubi/all/registration/client/RegistrationServiceClient.java
+++ src/main/java/io/qiot/ubi/all/registration/client/RegistrationServiceClient.java
@@ -5,9 +5,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.qiot.ubi.all.registration.domain.CAIssuerRequest;
 import io.qiot.ubi.all.registration.domain.CertificateRequest;
 import io.qiot.ubi.all.registration.domain.CertificateResponse;
 
@@ -28,5 +30,11 @@ public interface RegistrationServiceClient {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public CertificateResponse provisionCertificate(CertificateRequest request);
+
+    @Path("/issuer")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response provisionIssuer(CAIssuerRequest issuerRequest);
 
 }
